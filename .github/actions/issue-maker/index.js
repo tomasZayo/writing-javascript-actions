6e83b7c6db1df9fe5f3042826a7580c0d941c072
tomasZayo <95404981+tomasZayo@users.noreply.github.com>
2021-12-17 14:07:22 +0100

    action change

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,5 +1,5 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+
 
 async function run() {
   try {
@@ -7,13 +7,11 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = new github.getOctokit(token);
 
-    const newIssue = await octokit.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
+      console.log(issueTitle);
+      console.log(jokeBody);
+      console.log(token);
+
     });
   } catch (error) {
     core.setFailed(error.message);
